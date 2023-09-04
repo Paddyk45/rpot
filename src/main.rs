a9625d2a3a38b3c7daced328c9e09a8d8602d8fd
Patrick <patrickk0@tutanota.com>
2023-09-04 15:08:58 +0200

    Add some commands

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod model;
 
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::process::Command;
 use std::thread;
 
 use crate::model::*;
@@ -47,7 +48,7 @@ fn handle_client(mut stream: TcpStream) {
                     packet.length.unwrap(),
                     packet.request_id,
                     packet.packet_type,
-                    packet.payload.unwrap_or("empty".to_string())
+                    packet.payload.clone().unwrap_or("empty".to_string())
                 );
                 match packet.packet_type {
                     PacketType::Login => {
@@ -56,9 +57,25 @@ fn handle_client(mut stream: TcpStream) {
                     }
 
                     PacketType::RunCommand => {
-                        let response_packet =
-                            Packet::gen_response(packet.request_id, "".to_string());
-                        stream.write(&response_packet.to_u8_arr()).unwrap();
+                        let command = packet.payload.unwrap_or("".to_string());
+
+                        let command_response =
+                            match command.as_str().split_whitespace().next().unwrap_or("") {
+                                "seed" => "Seed: [69420]",
+                                "say" => "",
+                                "" => "",
+                                _ => "Unknown command. Type \"/help\" for help.",
+                            };
+
+                        stream
+                            .write(
+                                &Packet::gen_response(
+                                    packet.request_id.clone(),
+                                    command_response.to_string(),
+                                )
+                                .to_u8_arr(),
+                            )
+                            .expect("Failed to write to stream");
                     }
 
                     _ => println!("Client sent invalid packet type"),
